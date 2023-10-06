21af38e7302a1b0c7404835f5f1790c5bb2cb49e
Kyle Ancheta <kyleancheta@live.com>
2023-10-06 03:39:21 -0700

    changed da font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/global.css';
+import { Space_Grotesk } from 'next/font/google'
+
+const space_grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-space-grotesk',
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+    <main className={space_grotesk.className}>
+      <Component {...pageProps} />
+    </main>
+    )
   }
